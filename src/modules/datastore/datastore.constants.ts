0a7c8cf991c4d9aa6918169de1f72ac539360c23
Leander Paul <leanderpaul97@outlook.com>
2025-12-21 14:13:33 +0530

    feat(config): add sender profile, endpoint & assignment CRUD APIs

--- src/modules/datastore/datastore.constants.ts
+++ src/modules/datastore/datastore.constants.ts
@@ -20,4 +20,5 @@ export const constraintErrorMap: Record<string, ServerError> = {
   template_groups_template_key_unique: new ServerError(AppErrorCode.TPL_GRP_002),
   template_variants_template_group_id_template_groups_id_fk: new ServerError(AppErrorCode.TPL_GRP_001),
   template_variants_template_group_id_channel_locale_unique: new ServerError(AppErrorCode.TPL_VRT_002),
+  sender_profile_assignments_sender_profile_id_sender_profiles_id: new ServerError(AppErrorCode.SND_PRF_003),
 };
---
+++ src/routes/configuration/dtos/sender-assignment.dto.ts
@@ -0,0 +1,78 @@
+/**
+ * Importing npm packages
+ */
+import { Field, PickType, Schema } from '@shadow-library/class-schema';
+import { Transform } from '@shadow-library/fastify';
+import { Paginated, PaginationQuery } from '@shadow-library/modules/http-core';
+
+/**
+ * Importing user defined packages
+ */
+import { type Template, schema } from '@modules/datastore';
+
+import { SenderProfileResponse } from './sender-profile.dto';
+
+/**
+ * Declaring the constants
+ */
+
+@Schema()
+export class CreateSenderAssignmentBody {
+  @Field(() => String)
+  @Transform('bigint:parse')
+  senderProfileId: bigint;
+
+  @Field(() => String, { enum: schema.messageTypes.enumValues })
+  messageType: Template.MessageType;
+
+  @Field({ optional: true })
+  region?: string;
+
+  @Field({ optional: true })
+  serviceName?: string;
+}
+
+@Schema()
+export class SenderAssignmentResponse extends CreateSenderAssignmentBody {
+  @Field(() => String, { format: 'date-time' })
+  createdAt: Date;
+
+  @Field(() => String, { format: 'date-time' })
+  updatedAt: Date;
+}
+
+@Schema()
+export class SenderAssignmentDetailResponse extends SenderAssignmentResponse {
+  @Field()
+  profile: SenderProfileResponse;
+}
+
+@Schema()
+export class UpdateSenderAssignmentBody extends PickType(CreateSenderAssignmentBody, ['senderProfileId']) {}
+
+@Schema()
+export class ListSenderAssignmentsQuery extends PaginationQuery(['createdAt', 'updatedAt'] as const) {
+  @Field(() => String, { enum: schema.messageTypes.enumValues, optional: true })
+  messageType?: Template.MessageType;
+
+  @Field({ optional: true })
+  region?: string;
+
+  @Field({ optional: true })
+  serviceName?: string;
+}
+
+@Schema()
+export class SenderAssignmentParams {
+  @Field()
+  serviceName: string;
+
+  @Field(() => String, { enum: schema.messageTypes.enumValues })
+  messageType: Template.MessageType;
+
+  @Field()
+  region: string;
+}
+
+@Schema()
+export class ListSenderAssignmentResponse extends Paginated(SenderAssignmentResponse) {}
