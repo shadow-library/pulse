06a056c535fc15e1e25a91f376e72492c4985d79
Leander Paul <leanderpaul97@outlook.com>
2026-03-17 22:23:54 +0400

    refactor(modules): extract dynamic modules and bump shadow-library deps

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,12 @@ import './bootstrap';
  * Importing npm packages
  */
 import { Module } from '@shadow-library/app';
-import { FastifyModule } from '@shadow-library/fastify';
-import { HttpCoreModule } from '@shadow-library/modules';
 
 /**
  * Importing user defined packages
  */
-import { ConfigurationModule } from '@modules/configuration';
-import { MetricsModule } from '@modules/metrics';
-import { NotificationModule } from '@modules/notification';
-import { TemplateModule } from '@modules/template';
+import { HttpRouteModule } from '@modules/dynamic.modules';
 
-import { CUSTOM_DATA_TRANSFORMERS } from './common';
 import { DatabaseModule } from './database';
 
 /**
@@ -28,23 +22,6 @@ import { DatabaseModule } from './database';
 /**
  * Declaring the constants
  */
-const AppHttpCoreModule = HttpCoreModule.forRoot({
-  csrf: {
-    disabled: true,
-  },
-
-  openapi: {
-    normalizeSchemaIds: true,
-  },
-});
-
-const HttpRouteModule = FastifyModule.forRoot({
-  imports: [AppHttpCoreModule, ConfigurationModule, NotificationModule, TemplateModule, MetricsModule],
-
-  routePrefix: '/api',
-  prefixVersioning: true,
-  transformers: CUSTOM_DATA_TRANSFORMERS,
-});
 
 @Module({
   imports: [DatabaseModule, HttpRouteModule],
---
+++ src/modules/dynamic.modules.ts
@@ -0,0 +1,39 @@
+/**
+ * Importing npm packages
+ */
+import { FastifyModule } from '@shadow-library/fastify';
+import { HttpCoreModule } from '@shadow-library/modules';
+
+/**
+ * Importing user defined packages
+ */
+import { ConfigurationModule } from '@modules/configuration';
+import { MetricsModule } from '@modules/metrics';
+import { NotificationModule } from '@modules/notification';
+import { TemplateModule } from '@modules/template';
+import { CUSTOM_DATA_TRANSFORMERS } from '@server/common';
+
+/**
+ * Defining types
+ */
+
+/**
+ * Declaring the constants
+ */
+export const AppHttpCoreModule = HttpCoreModule.forRoot({
+  csrf: {
+    disabled: true,
+  },
+
+  openapi: {
+    normalizeSchemaIds: true,
+  },
+});
+
+export const HttpRouteModule = FastifyModule.forRoot({
+  imports: [AppHttpCoreModule, ConfigurationModule, NotificationModule, TemplateModule, MetricsModule],
+
+  routePrefix: '/api',
+  prefixVersioning: true,
+  transformers: CUSTOM_DATA_TRANSFORMERS,
+});
